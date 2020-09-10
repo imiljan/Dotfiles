bd79246ddade9d231e9708f68b8c08a75d62f78c
Miljan <miljan.ignjatovic.96@gmail.com>
2020-09-10 12:19:39 +0200

    Updates...

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 ############################
 # .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+# This script creates symlinks from the home directory to any desired dotfiles in ~/Dotfiles
 ############################
 
 ########## Variables
 
-dir=~/Dotfiles                               # dotfiles directory
-olddir=~/Dotfiles_old                        # old dotfiles backup directory
-files="zshrc profile gitconfig tmux.conf"    # list of files/folders to symlink in homedir
+dir=~/Dotfiles                                             # dotfiles directory
+olddir=~/Dotfiles_old                                      # old dotfiles backup directory
+files="zshrc profile gitconfig tmux.conf vimrc ideavimrc"  # list of files/folders to symlink in homedir
 
 ##########
 
@@ -25,51 +25,46 @@ echo "done"
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    mv ~/.$file ~/Dotfiles_old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
 
-if [[ $(echo $SHELL) == $(which bash) ]]; then
-    echo "source $HOME/.profile" >> ~/.bashrc
-fi
+ install_zsh () {
+ # Test to see if zshell is installed.  If it is:
+ if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
+     # Clone my oh-my-zsh repository from GitHub only if it isn't already present
+     if [[ ! -d ~/.oh-my-zsh/ ]]; then
+         git clone http://github.com/robbyrussell/oh-my-zsh.git ~/
+     fi
+     # Set the default shell to zsh if it isn't currently set to zsh
+     if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+         chsh -s $(which zsh)
+     fi
+ else
+     # If zsh isn't installed, get the platform of the current machine
+     platform=$(uname);
+     # If the platform is Linux, try an apt-get to install zsh and then recurse
+     if [[ $platform == 'Linux' ]]; then
+         if [[ -f /etc/redhat-release ]]; then
+             sudo yum install zsh
+             install_zsh
+         fi
+         if [[ -f /etc/debian_version ]]; then
+             sudo apt-get install zsh
+             install_zsh
+         fi
+     # If the platform is OS X, tell the user to install zsh :)
+     elif [[ $platform == 'Darwin' ]]; then
+         echo "Please install zsh, then re-run this script!"
+         exit
+     fi
+ fi
+ }
 
 
-# install_zsh () {
-# # Test to see if zshell is installed.  If it is:
-# if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-#     # Clone my oh-my-zsh repository from GitHub only if it isn't already present
-#     if [[ ! -d $dir/oh-my-zsh/ ]]; then
-#         git clone http://github.com/robbyrussell/oh-my-zsh.git
-#     fi
-#     # Set the default shell to zsh if it isn't currently set to zsh
-#     if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
-#         chsh -s $(which zsh)
-#     fi
-# else
-#     # If zsh isn't installed, get the platform of the current machine
-#     platform=$(uname);
-#     # If the platform is Linux, try an apt-get to install zsh and then recurse
-#     if [[ $platform == 'Linux' ]]; then
-#         if [[ -f /etc/redhat-release ]]; then
-#             sudo yum install zsh
-#             install_zsh
-#         fi
-#         if [[ -f /etc/debian_version ]]; then
-#             sudo apt-get install zsh
-#             install_zsh
-#         fi
-#     # If the platform is OS X, tell the user to install zsh :)
-#     elif [[ $platform == 'Darwin' ]]; then
-#         echo "Please install zsh, then re-run this script!"
-#         exit
-#     fi
-# fi
-# }
-
-
-# install_zsh
+install_zsh
 
 # Add zsh plugins
-# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
